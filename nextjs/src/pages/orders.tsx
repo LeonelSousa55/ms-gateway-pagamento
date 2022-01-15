4de045514247a308b75fc1cf05d53a7c57013776
Leonel <leonel55sousa@gmail.com>
2022-01-15 17:39:07 -0300

    feat: add mui style at project

--- nextjs/src/pages/orders.tsx
+++ nextjs/src/pages/orders.tsx
@@ -1,11 +1,13 @@
 import { GetServerSideProps } from "next";
 import axios from "axios";
+import { Button } from "@mui/material";
 
 const OrdersPage = (props: any) => {
     console.log(props.orders)
     return (
         <div>
             Listagens de orders...{props.name}
+            <Button variant="contained">Contained</Button>
         </div>
     );
 };
@@ -14,7 +16,11 @@ export default OrdersPage;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 
-    const {data} = await axios.get('http//localhost:3000/orders');
+    const { data } = await axios.get('http://localhost:3000/orders', {
+        headers: {
+            'x-token': 'wm9jhrn14l'
+        }
+    });
 
     return {
         props: {
